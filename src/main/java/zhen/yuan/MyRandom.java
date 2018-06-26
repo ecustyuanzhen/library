63b39d0ee025039e71f7edc56cec735a684b0770
ecustyuanzhen <34464582+ecustyuanzhen@users.noreply.github.com>
2018-06-26 22:30:45 +0800

    update myPrint

--- src/main/java/zhen/yuan/MyRandom.java
+++ src/main/java/zhen/yuan/MyRandom.java
@@ -1,26 +1,59 @@
 package zhen.yuan;
 
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 /**
  * 随机数工具类
- *
  */
-public class MyRandom
-{
+public class MyRandom {
     private static Random random = new Random();
 
     /**
      * 获得指定大小的随机数组
+     *
      * @param size
      * @param max
      * @return
      */
-    public static int[] getRandomArray(int size,int max){
-        int[] array=new int[size];
-        for (int i=0;i<size;i++){
-            array[i]=random.nextInt(max);
+    public static int[] getRandomArray(int size, int max) {
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt(max);
         }
         return array;
+    }
+
+    public static int[] getRandomArrayUnrepeat(int size, int max) {
+        int[] array = new int[size];
+        Set<Integer> set = new HashSet<>();
+        int count = 0, temp;
+        while (count < 10) {
+            temp = random.nextInt(max);
+            if (set.contains(temp)) continue;
+            array[count] = temp;
+            count++;
+        }
+        return array;
+    }
+
+    /**
+     * get random with no param
+     *
+     * @return
+     */
+    public static int getRandom() {
+        return random.nextInt();
+    }
+
+    /**
+     * get random with param
+     *
+     * @param i
+     * @return
+     */
+    public static int getRandom(int i) {
+        return random.nextInt(i);
     }
 }
